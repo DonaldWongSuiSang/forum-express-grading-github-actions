6fe770b9f88120d57b2dc59ef06f9ed5adf62759
donaldwongsuisang <donaldwongsuisang@gmail.com>
2024-05-23 14:10:18 +0800

    adding dashboard R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,12 +18,23 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      if (!restaurant) throw new Error('Restaurant did not exist!')
+      return restaurant.increment('viewCounts')
+    })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       include: Category,
       nest: true,
       raw: true
     }).then(restaurant => {
+      console.log(restaurant)
       if (!restaurant) throw new Error('Restaurant did not exist!')
-      return res.render('restaurant', { restaurant })
+      return res.render('dashboard', { restaurant })
     })
       .catch(err => next(err))
   }
